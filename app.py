948a767d97dda4ccf623b4f6765577deb036cd4f
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2021-09-27 00:56:46 +0200

    Fix stupid Simple HTTP server and add http auth

--- app.py
+++ app.py
@@ -3,15 +3,28 @@
 import socketserver
 import http.server
 from influxdb import InfluxDBClient
+from base64 import b64encode
+import os
 
 client = InfluxDBClient('influxdb', database='mydb')
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_POST(self):
+basic_value = b64encode(bytes('%s:%s' % (os.environ['USERNAME'], os.environ['PASSWORD']), 'utf-8')).decode('ascii')
 
-        if (self.path != '/ping'):
-          self.send_response(404)
-          return
+class Handler(http.server.BaseHTTPRequestHandler):
+    def do_POST(self):
+        print('Receive request')
+
+        if self.headers.get('Authorization') != 'Basic ' + basic_value:
+
+            self.send_response(401)
+            self.send_header('WWW-Authenticate', 'Basic realm="I alive"')
+            self.end_headers()
+            return
+
+        if self.path != '/ping':
+            self.send_response(404)
+            self.end_headers()
+            return
 
         print('ping !')
 
@@ -27,7 +40,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.send_response(201)
         self.end_headers()
 
-httpd = socketserver.TCPServer(('', 8080), Handler)
+httpd = socketserver.TCPServer(('', os.environ.get('PORT', 80)), Handler)
 try:
    httpd.serve_forever()
 except KeyboardInterrupt:
